3469de6a8e4a36d6efec26d0a4bb5494f5c175d3
Max Dubovets <maximque.d@gmail.com>
2021-03-25 00:06:51 +0300

    fix: added locality of names according to rule 3.3 in html-and-css.md

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.querySelector("#in-complete-tasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.querySelector("#execute");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("performed");//completed-tasks
 
 
 //New task list item
